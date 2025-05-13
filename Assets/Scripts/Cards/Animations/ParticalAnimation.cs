f83cb6b3bb3c432274534da1d029b90d13e33e23
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2025-05-14 02:15:42 +0800

    fix: partical layer

--- Assets/Scripts/Cards/Animations/ParticalAnimation.cs
+++ Assets/Scripts/Cards/Animations/ParticalAnimation.cs
@@ -6,13 +6,13 @@ namespace Cards.Animations {
     class ParticalAnimation {
         public static void PlayCardAnimation(GameObject target, CardAnimations animationName) {
             GameObject effect = CardAnimationLoader.LoadCardAnimator();
-            effect.transform.position = target.transform.position;
+            effect.transform.position = target.transform.position + new Vector3(0, 0, -1.5f);
             effect.transform.SetParent(target.transform);
 
             Animator animator = effect.GetComponentInChildren<Animator>();
             animator.runtimeAnimatorController = CardAnimationLoader.LoadCardAnimation(animationName);
             AnimationClip clip = animator.runtimeAnimatorController.animationClips[0];
-            GameObject.Destroy(effect, clip.length + 0.2f);
+            // GameObject.Destroy(effect, clip.length + 0.2f);
         }
     }
 }
