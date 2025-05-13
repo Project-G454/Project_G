d99eb23cb4efb5a11dabd289ac709bf1aa63e871
JackyJL <132221068+JackyJL@users.noreply.github.com>
2025-05-13 09:21:42 +0800

    add sprites to cover basic color of wall and ground

--- Assets/Scripts/Managers/GameManagers/GridManager.cs
+++ Assets/Scripts/Managers/GameManagers/GridManager.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 namespace Core.Managers {
     public class GridManager : MonoBehaviour, IManager {
         public static GridManager Instance;
+
         [SerializeField] private int _width, _height;
         [SerializeField] private Tile _tilePrefab;
-        [SerializeField] private Tile _obstacleTilePrefab; 
+        [SerializeField] private Tile _obstacleTilePrefab;
         [SerializeField] private Color floorColor, wallColor;
-        
+
+        [Header("Sprite Overlay")]
+        [SerializeField] private Sprite floorSprite;
+        [SerializeField] private Sprite wallSprite;
+
         [Header("Map Generation")]
         [SerializeField] private int _boundaryOffset = 1;
         [SerializeField] private RandomWalkGeneratorData _randomWalkData;
@@ -22,66 +27,58 @@ namespace Core.Managers {
 
         public int width => _width;
         public int height => _height;
-        
+
         void Awake() {
             if (Instance != null && Instance != this) {
                 Destroy(gameObject);
                 return;
             }
+
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
 
         public void Init() {
             _cam = Camera.main.transform;
-            // 創建地圖生成器
             _mapGenerator = new RandomWalkGenerator(_randomWalkData);
         }
 
         public void GenerateGrid() {
             tiles = new Dictionary<Vector2, Tile>();
-            
-            // 使用生成器生成地圖數據
             Vector2Int roomCenter = new Vector2Int(_width / 2, _height / 2);
-            
             HashSet<Vector2Int> floorPositions = _mapGenerator.GenerateMap(_width, _height, roomCenter, _boundaryOffset);
-                
-            CreateTilesFromFloorPositions(floorPositions); // 視覺化地圖
-            
+            CreateTilesFromFloorPositions(floorPositions);
             _cam.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
         }
 
         private void CreateTilesFromFloorPositions(HashSet<Vector2Int> floorPositions) {
-            // 創建在地板位置上的Tile
             foreach (var pos in floorPositions) {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector2(pos.x, pos.y), Quaternion.identity, map);
                 spawnedTile.name = $"Tile {pos.x} {pos.y}";
-                
                 spawnedTile.Init(new Vector2(pos.x, pos.y));
 
-                SpriteRenderer spriteRenderer = spawnedTile.GetComponent<SpriteRenderer>();
-                if (spriteRenderer != null) {
-                    spriteRenderer.color = floorColor;
+                SpriteRenderer sr = spawnedTile.GetComponent<SpriteRenderer>();
+                if (sr != null) {
+                    sr.sprite = floorSprite; // ✅ 改為拖入 sprite
+                    sr.color = floorColor;   // ✅ 疊加顏色
                 }
-                
+
                 tiles[new Vector2(pos.x, pos.y)] = spawnedTile;
             }
-            
-            // 創建牆壁
+
             HashSet<Vector2Int> wallPositions = FindWallPositions(floorPositions);
             foreach (var pos in wallPositions) {
-                // 確保在網格範圍內
                 if (pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height) {
                     Tile tileToUse = _obstacleTilePrefab != null ? _obstacleTilePrefab : _tilePrefab;
                     var spawnedTile = Instantiate(tileToUse, new Vector2(pos.x, pos.y), Quaternion.identity, map);
                     spawnedTile.name = $"Wall {pos.x} {pos.y}";
-                    
                     spawnedTile.Init(new Vector2(pos.x, pos.y));
-                    spawnedTile.SetWalkable(false); // 將牆壁設為不可走
-                    
-                    SpriteRenderer spriteRenderer = spawnedTile.GetComponent<SpriteRenderer>();
-                    if (spriteRenderer != null) {
-                        spriteRenderer.color = wallColor;
+                    spawnedTile.SetWalkable(false);
+
+                    SpriteRenderer sr = spawnedTile.GetComponent<SpriteRenderer>();
+                    if (sr != null) {
+                        sr.sprite = wallSprite; // ✅ 改為拖入 sprite
+                        sr.color = wallColor;   // ✅ 疊加顏色
                     }
 
                     tiles[new Vector2(pos.x, pos.y)] = spawnedTile;
@@ -91,15 +88,11 @@ namespace Core.Managers {
 
         private HashSet<Vector2Int> FindWallPositions(HashSet<Vector2Int> floorPositions) {
             HashSet<Vector2Int> wallPositions = new HashSet<Vector2Int>();
-            
-            List<Vector2Int> directions = new List<Vector2Int>
-            {
-                new Vector2Int(0, 1),  // 上
-                new Vector2Int(1, 0),  // 右
-                new Vector2Int(0, -1), // 下
-                new Vector2Int(-1, 0)  // 左
+            List<Vector2Int> directions = new List<Vector2Int> {
+                new Vector2Int(0, 1), new Vector2Int(1, 0),
+                new Vector2Int(0, -1), new Vector2Int(-1, 0)
             };
-            
+
             foreach (var pos in floorPositions) {
                 foreach (var dir in directions) {
                     var neighborPos = pos + dir;
@@ -108,10 +101,9 @@ namespace Core.Managers {
                     }
                 }
             }
-            
+
             return wallPositions;
         }
-
 
         public Tile GetTileAtPosition(Vector2 pos) {
             if (tiles.TryGetValue(pos, out var tile)) {
@@ -132,7 +124,7 @@ namespace Core.Managers {
             int y = Mathf.RoundToInt(worldPosition.y);
             return new Vector2(x, y);
         }
-        
+
         public void ClearGrid() {
             if (tiles != null) {
                 foreach (var tile in tiles.Values) {
@@ -143,7 +135,7 @@ namespace Core.Managers {
                 tiles.Clear();
             }
         }
-        
+
         public void RegenerateGrid() {
             ClearGrid();
             GenerateGrid();
