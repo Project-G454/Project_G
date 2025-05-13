9535406679f1fbb13f29686154f051b6dcd68ab4
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2025-05-13 23:20:05 +0800

    Revert "Restructure spawn & random field logic"
    This reverts commit ad4fc788b95b0c55577a92df5bfa7ffa4b76d5a3.

--- Assets/Scripts/Core/Map/Generators/IMapGenerator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Core.Map.Generators {
-    // 地圖生成器接口
-    public interface IMapGenerator {
-        HashSet<Vector2Int> GenerateMap(int width, int height, Vector2Int startPosition, int boundaryOffset);
-    }
-}
