af4c0e753f51dd6ec8614af8749ac07f78cd54d0
JackyJL <132221068+JackyJL@users.noreply.github.com>
2025-06-03 21:13:08 +0800

    Player state update

--- Assets/Scripts/Managers/GameManagers/PlayerStateManager.cs
+++ Assets/Scripts/Managers/GameManagers/PlayerStateManager.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Core.Game;
 
 public class PlayerStateManager : MonoBehaviour {
     public static PlayerStateManager Instance { get; private set; }
 
-    public List<string> currentDeck = new List<string>(); // 可改成Card類型
-    public int currentHP = 100;
-    public int currentGold = 0;
+    [SerializeField] private List<GamePlayerState> allPlayers = new List<GamePlayerState>();
+    private Dictionary<string, GamePlayerState> players = new Dictionary<string, GamePlayerState>();
 
     private void Awake() {
         if (Instance != null && Instance != this) {
@@ -18,15 +18,38 @@ public class PlayerStateManager : MonoBehaviour {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void SetDeck(List<string> deck) {
-        currentDeck = new List<string>(deck);
+    public void AddPlayer(string id, List<string> deck, int hp = 100, int gold = 0) {
+        if (!players.ContainsKey(id)) {
+            var state = new GamePlayerState(id, deck, hp, gold);
+            players[id] = state;
+            allPlayers.Add(state);
+        }
     }
 
-    public void ModifyHP(int amount) {
-        currentHP += amount;
+    public GamePlayerState GetPlayer(string id) {
+        return players.ContainsKey(id) ? players[id] : null;
     }
 
-    public void ModifyGold(int amount) {
-        currentGold += amount;
+    public void ModifyHP(string id, int delta) {
+        if (players.ContainsKey(id)) {
+            players[id].hp += delta;
+        }
+    }
+
+    public void ModifyGold(string id, int delta) {
+        if (players.ContainsKey(id)) {
+            players[id].gold += delta;
+        }
+    }
+
+    public void SetDeck(string id, List<string> newDeck) {
+        if (players.ContainsKey(id)) {
+            players[id].deck = new List<string>(newDeck);
+        }
+    }
+
+    public void ClearAllPlayers() {
+        players.Clear();
+        allPlayers.Clear();
     }
 }
